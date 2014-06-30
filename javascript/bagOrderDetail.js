c8cf11945f19f41cb16a22c5421e732f880c069a
steven <sikaco0@gmail.com>
2014-06-30 16:04:37 +0800

    update

---
+++ javascript/bagOrderDetail.js
@@ -0,0 +1,12 @@
+$(function() {
+  var submitBtn = document.getElementById("orderSubmit");
+  var form = $('#'+submitBtn.dataset.formid);
+  submitBtn.onclick = function() {
+    modalBoxPopup(form);
+  };
+  var confirmBtn =
+});
+
+function modalBoxPopup(form) {
+
+}
