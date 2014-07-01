7fb6fa997dcc75885c19ea50d619f7853d563217
steven <sikaco0@gmail.com>
2014-07-01 18:46:49 +0800

    up

--- javascript/bagOrderDetail.js
+++ javascript/bagOrderDetail.js
@@ -1,13 +1,21 @@
-function modalBoxPopup(obj) {
-  var form = document.getElementById(obj.dataset.formid);
+var modalBox = {
+  /*obj为触发模态框弹出按钮*/
+  exec: function(obj) {
+    var form = document.getElementById(obj.dataset.formid);
+    this.contentFill(form);
+    this.popup(form);
+  },
+  contentFill: function(form) {
 
-}
+  },
+  popup: function(form) {
 
-function modalBoxContentFill(form) {
+  }
+};
 
-  popup();
-}
-
-function popup() {
-
-}
+$(function() {
+  var submit = document.getElementById("orderSubmit");
+  submit.addEventListener('click', function() {
+    modalBox.exec(this);
+  });
+});
